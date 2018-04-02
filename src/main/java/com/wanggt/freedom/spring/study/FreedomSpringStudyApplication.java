6d33fb68f7d3d51f3658898ed7c3a71458de6ae1
wang465745776 <465745776@qq.com>
2018-04-02 21:46:16 +0800

    update to spring boot project

---
+++ src/main/java/com/wanggt/freedom/spring/study/FreedomSpringStudyApplication.java
@@ -0,0 +1,12 @@
+package com.wanggt.freedom.spring.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreedomSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreedomSpringStudyApplication.class, args);
+	}
+}
--- src/main/java/com/wanggt/freedom/spring/study/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package com.wanggt.freedom.spring.study;
-
-public class HelloWorld {
-
-}
